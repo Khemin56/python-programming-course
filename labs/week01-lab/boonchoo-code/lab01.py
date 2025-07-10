fe204dad7eed69ca16f43e839547897d7a9b4473
Boonchoo Jitnupong <Jitnupong khemin.c@ku.th>
2025-07-10 15:10:32 +0700

    add_lab01.py

---
+++ labs/week01-lab/boonchoo-code/lab01.py
@@ -0,0 +1,17 @@
+# Exercise 1: personal Profile Creator
+print("=== Exercise 1: Basic Input ===")
+user_name = input("What is your name? ")
+print("Hello,", user_name, "!")
+print("Nice to meet you,", user_name)
+#write your solution here:
+full_name = input("What is your name?:")
+age= input("Hoe old are you?")
+email = input("What is your email address?:")
+phone_number = input("What is your phone number?:")
+hobby =input ("What is your favorite hobby? :")
+print("===You profile===")
+print("Full name:"full_name)
+print("age:",age)
+print("email:"email)
+print("phone number:",phone_number)
+print("favorite hobby? :",hobby)
